d2532beb3f6d7e198dd6e69686bda53e2c710f47
Asaolu Paul <asaolupaul017@gmail.com>
2022-10-13 16:07:51 -0700

    C is compiled

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: by default returns zero if no errors otherwise returns non-zero value
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return (0)
+}
