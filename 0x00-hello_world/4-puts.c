4c4a88f083392fb004f9b3373b7553ad55ab8353
Asaolu Paul <asaolupaul017@gmail.com>
2022-10-13 19:15:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * Return: by default returns zero if no errors otherwise returns non-zero value
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-		return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
