5d1d64187b9d70379920c431086394407dade400
Asaolu Paul <asaolupaul017@gmail.com>
2022-10-24 20:13:03 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,18 +2,14 @@
 
 /**
  * _puts - prints a string, followed by a new line to stdout
- *
  * @str: string parameter to print
- *
- * Return: Nothing
  */
 
 void _puts(char *str)
 {
 	while (*str !=  '\0')
 	{
-		_putchar(*str + 0);
-		++str;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
